1ad1192f7789f551b4af12f019b6ccae4e98bccf
Judgem3ntal <jakeit47@yahoo.com>
2013-01-22 09:37:31 -0500

    Update com/katie/game/tilegame/sprites/Plant.java
    Created framework for a new monster. 1/22/2013 9:37 AM EST

--- com/katie/game/tilegame/sprites/Plant.java
+++ com/katie/game/tilegame/sprites/Plant.java
@@ -1,4 +1,21 @@
-package com.katie.game.tilegame.sprites;
-import com.katie.game.graphics.Animation;
-  public class Plant extends Creature
-  
+/*    */ package com.katie.game.tilegame.sprites;
+/*    */ 
+/*    */ import com.katie.game.graphics.Animation;
+/*    */ 
+/*    */ public class Plant extends Creature
+/*    */ {
+/*    */   public Ground(Animation left, Animation right, Animation deadLeft, Animation deadRight)
+/*    */   {
+/* 13 */     super(left, right, deadLeft, deadRight);
+/*    */   }
+/*    */ 
+/*    */   public float getMaxSpeed()
+/*    */   {
+/* 18 */     return 0.0F;
+/*    */   }
+/*    */ }
+
+/* Location:           C:\Users\Jake Thornton\Desktop\OddBall.jar
+ * Qualified Name:     com.katie.game.tilegame.sprites.Plant
+ * JD-Core Version:    0.6.0
+ */
